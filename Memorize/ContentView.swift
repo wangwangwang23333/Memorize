36248dc4b59a21a0160539a25b69be0dbcc7cfd8
汪汪汪 <1209497940@qq.com>
2022-10-05 14:04:30 +0800

    add scrollview

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -15,9 +15,12 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
-                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                    CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+            ScrollView{
+                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
                 }
             }
             .foregroundColor(.red)
