2769d50d4283d894cdcb86afb12f4780878c41b7
汪汪汪 <1209497940@qq.com>
2022-10-07 20:09:15 +0800

    use static

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 
 class EmojiMemoryGame {
-    private var model: MemoryGame<String> =
-    MemoryGame<String>(numberOfPairsOfCards: 4) { _ in "😄" }
+    
+    static let emojis = ["🛴", "🚲", "🛵", "🏍", "🛺", "🚔", "🚍", "🚘", "🚖", "🚡", "🚃", "🚝", "🚂", "🚆", "🚇", "✈️", "🛫", "🚀", "🛸", "🚁", "⛵️", "🚤", "🚢", "🛳"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
